9ca14d9261f77e25b12e2deede13c5876f6d8762
patryk <gryzabawy@o2.pl>
2025-04-27 11:44:41 +0200

    adapt setup.py to get_var_type_or_die function

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 import os
 from dotenv import load_dotenv
+from typing import Optional
+
+from utils.setup_validator import get_var_type_or_die
 
 
 load_dotenv(override=True)
 
-AZURE_OPENAI_API_KEY: str = os.getenv('AZURE_OPENAI_API_KEY')
-AZURE_OPENAI_API_ENDPOINT: str = os.getenv('AZURE_OPENAI_API_ENDPOINT')
-AZURE_OPENAI_API_VERSION: str = os.getenv('AZURE_OPENAI_API_VERSION')
-AZURE_OPENAI_MODEL: str = os.getenv('AZURE_OPENAI_MODEL')
-TIKTOKEN_MODEL: str = os.getenv('TIKTOKEN_MODEL')
-MAX_TOKENS: int = int(os.getenv('MAX_TOKENS'))
-TEMPERATURE: float = float(os.getenv('TEMPERATURE'))
+AZURE_OPENAI_API_KEY: str = get_var_type_or_die('AZURE_OPENAI_API_KEY', str)
+AZURE_OPENAI_API_ENDPOINT: str = get_var_type_or_die('AZURE_OPENAI_API_ENDPOINT', str)
+AZURE_OPENAI_API_VERSION: str = get_var_type_or_die('AZURE_OPENAI_API_VERSION', str)
+AZURE_OPENAI_MODEL: str = get_var_type_or_die('AZURE_OPENAI_MODEL', str)
+TIKTOKEN_MODEL: str = get_var_type_or_die('TIKTOKEN_MODEL', str)
+
+MAX_TOKENS: Optional[int] = int(str(os.getenv('MAX_TOKENS'))) if os.getenv('MAX_TOKENS') else None
+TEMPERATURE: Optional[float] = float(str(os.getenv('TEMPERATURE'))) if os.getenv('TEMPERATURE') else None
